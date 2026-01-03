2daffa11c8dbf7e8d78bbbbdeeeaf24b76aabe84
Suzuka Funayama <suzuka-funayama@kalonade.co.jp>
2026-01-03 22:31:47 +0900

    change body null

--- go_todo_api/infrastructure/database/model/todo.go
+++ go_todo_api/infrastructure/database/model/todo.go
@@ -4,11 +4,12 @@ import (
 	"time"
 )
 
+// DBのテーブルの構造を定義
 type Todos struct {
 	ID          string     `gorm:"primaryKey;autoIncrement"`
 	UserID      string     `gorm:"index;type:varchar(100);not null"`
 	Title       string     `gorm:"type:varchar(100);not null"`
-	Body        string     `gorm:"type:text"`
+	Body        string     `gorm:"type:text;not null"`
 	DueDate     *time.Time `gorm:"type:datetime"`
 	IsCompleted bool       `gorm:"type:boolean;default:false"`
 	CreatedAt   time.Time  `gorm:"autoCreateTime" json:"created_at"`
